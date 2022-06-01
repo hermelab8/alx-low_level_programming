5f21c396603076aedbe10126875b92aa06fe716e
hermelab8 <hermelab8@gmail.com>
2022-06-01 15:54:34 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - puts() function
  *
- * Return : 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 
-	char Putstr[] = "\"Programming is like building a multilingual puzzle\"";
+	char Putstr[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Putstr);
 	return (0);
