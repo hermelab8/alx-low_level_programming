c88062ad69ae71f932f0dfd5229d22a36bd6429f
hermelab8 <hermelab8@gmail.com>
2022-06-03 19:57:43 +0000

    2 digit combo non repeating

---
+++ 0x01-variables_if_else_while/100-print_com3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - two digit with char combo
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int f, s;
+
+	for (f= 0; f <= 9; f++)
+	{
+		for (s = f + 1; s <= 9; s++)
+		{
+			putchar((f % 10) + '0');
+			putchar((s % 10) + '0');
+		
+			if (f == 8 && s == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+			
+		}
+	}
+	putchar('\n');
+	return (0);
+}
