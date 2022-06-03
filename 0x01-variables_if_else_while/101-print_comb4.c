2721437a2a884a0026e418eb9307da20f46af254
hermelab8 <hermelab8@gmail.com>
2022-06-03 20:46:14 +0000

    three digit combination

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - 3 digit combo of 0 1 2 without repeatition
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int f, s, t;
+
+	for (f = 0; f < 8; f++)
+	{
+		for (s = f + 1; s < 9; s++)
+		{
+			for (t = f + 2; t < 10; t++)
+			{
+				putchar((f % 10) + '0');
+				putchar((s % 10) + '0');
+				putchar((s % 10) + '0');
+				
+				if (f == 7 && s == 8 && t == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
