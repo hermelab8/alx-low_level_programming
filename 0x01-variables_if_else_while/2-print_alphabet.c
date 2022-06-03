d093eb6d466b968abe8e42dee7d858c629c5bd7b
hermelab8 <hermelab8@gmail.com>
2022-06-03 08:30:10 -0700

    print the lowecase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print the lowercase alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lcase;
+
+	for (lcase = 'a'; lcase <= 'z'; lcase++)
+	{
+		putchar(lcase);
+	}
+	return (0);
+}
