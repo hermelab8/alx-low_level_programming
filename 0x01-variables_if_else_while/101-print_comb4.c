66b6228c0aca4bc9ed0ca0c7321065129036f991
hermelab8 <hermelab8@gmail.com>
2022-06-03 20:51:16 +0000

    three digit combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,12 +13,12 @@ int main(void)
 	{
 		for (s = f + 1; s < 9; s++)
 		{
-			for (t = f + 2; t < 10; t++)
+			for (t = s + 1; t < 10; t++)
 			{
 				putchar((f % 10) + '0');
 				putchar((s % 10) + '0');
-				putchar((s % 10) + '0');
-				
+				putchar((t % 10) + '0');
+
 				if (f == 7 && s == 8 && t == 9)
 					continue;
 				putchar(',');
