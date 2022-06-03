62b3cd2d70e2dc7a24ebfb6709697b53d91a449f
hermelab8 <hermelab8@gmail.com>
2022-06-03 08:03:18 -0700

    printout last digit of a number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,15 +27,15 @@ int main(void)
 
 	if (LDigit > 5)
 	{
-		printf("Last digit of, %d, is, %d, and is greater than 5", n, LDigit);
+		printf("Last digit of %d is %d and is greater than 5", n, LDigit);
 	}
 	else if (LDigit == 0)
 	{
-		printf("Last digit of, %d, is, %d, and is 0", n, LDigit);
+		printf("Last digit of %d is %d and is 0", n, LDigit);
 	}
 	else
 	{
-		printf("Last digit of, %d, is, %d, and is less than 6 and not 0", n, LDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, LDigit);
 	}
 	return (0);
 }
