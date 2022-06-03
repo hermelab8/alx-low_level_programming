f7a1d0add6435c4993dba76a70d7d4d074afa9c9
hermelab8 <hermelab8@gmail.com>
2022-06-03 10:00:55 -0700

    print 0-9 with for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int s_num;
+
+	for (s_num = 0; s_num <= 9; s_num++)
+	{
+		printf("%d\n", s_num);
+	}
+	return (0);
+}
+
