528916836d9ca46549243900304f2b43a3562d19
hermelab8 <hermelab8@gmail.com>
2022-06-07 20:02:51 +0000

    print lower case alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *main- print_alphabet block has a void return type
- *Description: print_alphabet prints lowercase abc
- *Return: Always void (Success)
+ *print_alphabet - print_alphabet block has a void return type
+ *
+ *Return: void on success
  */
 void print_alphabet(void)
 {
