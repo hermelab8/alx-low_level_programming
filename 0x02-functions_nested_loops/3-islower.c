b6446bfd5aaa6e365865065324714c7c4c92423f
hermelab8 <hermelab8@gmail.com>
2022-06-07 20:55:05 +0000

    check whether lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower- checks lowercase character
+ *
+ * Return: 1 on success
+ */
+int _islower(int l)
+{
+	if (l >= 97 && l <= 122)
+	{
+	       return (1);
+	}
+	else
+        return (0);
+	
+}
