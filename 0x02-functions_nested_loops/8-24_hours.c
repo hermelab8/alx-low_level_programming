4919ab27b4b297842ab9681e20d38defb02b59f4
hermelab8 <hermelab8@gmail.com>
2022-06-07 23:01:28 +0000

    jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * jack_bauser- prints every minute of a day
+ * jack_bauer- prints every minute of a day
  *
  * Return: void on success
  */
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int Hh, Mm;
 
-	for ( Hh = 0; Hh < 24; Hh++)
+	for (Hh = 0; Hh < 24; Hh++)
 	{
-		for ( Mm = 0; Mm < 60; Mm++)
+		for (Mm = 0; Mm < 60; Mm++)
 		{
 			_putchar((Hh / 10) + '0');
 			_putchar((Hh % 10) + '0');
