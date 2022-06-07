1e02e9bbbe51cadd6229b3087c0339a5dcada9a5
hermelab8 <hermelab8@gmail.com>
2022-06-07 10:08:30 +0000

    functions and nested loops

--- 0x02-functions_nestes_loops/0-putchar.c
+++ 0x02-functions_nestes_loops/0-putchar.c
@@ -5,13 +5,14 @@
  *
  * Return: Always 0 (Success).
  */
-int main()
+int main(void)
 {
-	int print[] = (_putchar);
+	char print[8] = "_putchar";
+	int i;
 
 	for (i = 0; i < 8; i++)
 	{
-	_putchar print[i];
+	_putchar(print[i]);
 	}
 	_putchar('\n');
 	return (0);
---
+++ 0x02-functions_nestes_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main."
+
+/**
+ * main - uses the print_alphabet function to print lower case ABC
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int abc[] = (a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,x);
+	
+	for (i = 0; i < 26; i++)
+	{
+		print_alphabet abc[i];
+	}
+	_putchar('\n');
+	return (0);
+}
