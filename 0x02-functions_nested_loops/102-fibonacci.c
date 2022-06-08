9ed300839e9339194b606794fdd0445e11986a77
hermelab8 <hermelab8@gmail.com>
2022-06-08 10:51:00 +0000

    fib series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main- the first 50 terms in the fibonacci series
+ *
+ * Return: Always 0.
+ */
+int main()
+{
+	int c, current, next, fib;
+
+		current = 0;
+		next = 1;
+		fib = current + next;
+
+		for (c = 0; c < 50; c++)
+		{
+			if (c < 50)
+			{
+				printf("%d, ", fib);
+			}
+			else
+			{
+				printf("%d\n", fib);
+			}
+			current = next;
+			next = fib;
+
+			fib = current + next;
+
+			printf("%d, ", fib);
+		}
+	return (0);
+}
+
