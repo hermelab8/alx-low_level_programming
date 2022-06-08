07b7f43481801128db8760a3bbf7391e5f809ddf
hermelab8 <hermelab8@gmail.com>
2022-06-08 09:42:45 +0000

    sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,26 +7,17 @@
  */
 int main(void)
 {
-	int c,t_sum,f_sum,M_sum;
-
-	c = 0;
+	int c, M_sum;
 
 	for (c = 0; c < 1024; c++)
 	{
-		t_sum = 0;
-		f_sum = 0;
+		M_sum = 0;
 
-		if ((c % 3) == 0)
+		if ((c % 3) == 0 || (c % 5) == 0)
 		{
-			t_sum = c + t_sum;
+			M_sum = M_sum + c;
 		}
-		else if ((c % 5) == 0)
-		{
-			f_sum = c + f_sum;
-		}
-		else continue;
 	}
-	M_sum = t_sum + f_sum;
 	printf("%d\n", M_sum);
 	return (0);
 }
